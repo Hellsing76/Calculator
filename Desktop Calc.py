3370ab7e84dbe1091b72fdaa8c978c138c49d9d5
Nestor Fong <fong.nestor@yahoo.com>
2017-09-26 18:32:28 -0500

    added memory buttons and redesigned the project

--- Desktop Calc.py
+++ Desktop Calc.py
@@ -1,100 +1,69 @@
-from tkinter import *
+import Tkinter as tk
 
-master = Tk()
-display = Entry(master, width=11,justify='center',bd=0,bg='darkgreen')
 
-master.title("TI-Fong")
+def click(key):
+    global memory
+    if key == '=':
+        # avoid division by integer
+        if '/' in entry.get() and '.' not in entry.get():
+            entry.insert(tk.END, ".0")
+        # guard against the bad guys abusing eval()
+        str1 = "-+0123456789."
+        if entry.get()[0] not in str1:
+            entry.insert(tk.END, "first char not in " + str1)
+        # here comes the calculation part
+        try:
+            result = eval(entry.get())
+            entry.insert(tk.END, " = " + str(result))
+        except:
+            entry.insert(tk.END, "--> Error!")
+    elif key == 'C':
+        entry.delete(0, tk.END)  # clear entry
+    elif key == '->M':
+        memory = entry.get()
+        # extract the result
+        if '=' in memory:
+            ix = memory.find('=')
+            memory = memory[ix + 2:]
+        root.title('M=' + memory)
+    elif key == 'M->':
+        entry.insert(tk.END, memory)
+    elif key == 'neg':
+        if '=' in entry.get():
+            entry.delete(0, tk.END)
+        try:
+            if entry.get()[0] == '-':
+                entry.delete(0)
+            else:
+                entry.insert(0, '-')
+        except IndexError:
+            pass
+    else:
+        # previous calculation has been done, clear entry
+        if '=' in entry.get():
+            entry.delete(0, tk.END)
+        entry.insert(tk.END, key)
 
-class Calculator:
 
-    def __init__(self):
-        self.var1 = ""
-        self.var2 = ""
-        self.result = 0
-        self.current = 0
-        self.operator = 0
-
-    def numb_butt(self, index):
-        if self.current is 0:
-            self.var1 = "{0}{1}".format(str(self.var1), str(index))
-            display.delete(0, END)
-            display.insert(0, string=self.var1)
-        else:
-            self.var2 = "{0}{1}".format(str(self.var2), str(index))
-            display.delete(0, END)
-            display.insert(0, string=self.var2)
-
-    def equate(self):
-        if self.operator is 0:
-            self.result = float(self.var1) + float(self.var2)
-        elif self.operator is 1:
-            self.result = float(self.var1) - float(self.var2)
-        elif self.operator is 2:
-            self.result = float(self.var1) * float(self.var2)
-        elif self.operator is 3:
-            self.result = float(self.var1) / float(self.var2)
-        elif self.operator is 4:
-            self.result = float(self.var1) ** float(self.var2)
-        display.delete(0, END)
-        display.insert(0, string=self.result)
-
-    def set_op(self, op):
-        self.operator = op
-        display.delete(0, END)
-        if self.current is 0:
-            self.current = 1
-        else:
-            self.equate()
-            self.var2 = ""
-
-    def clear(self):
-        self.__init__()
-        display.delete(0, END)
-
-calc = Calculator()
-
-b0 = Button(master, text='0', command=lambda: calc.numb_butt(0))
-b1 = Button(master, text='1', command=lambda: calc.numb_butt(1))
-b2 = Button(master, text='2', command=lambda: calc.numb_butt(2))
-b3 = Button(master, text='3', command=lambda: calc.numb_butt(3))
-b4 = Button(master, text='4', command=lambda: calc.numb_butt(4))
-b5 = Button(master, text='5', command=lambda: calc.numb_butt(5))
-b6 = Button(master, text='6', command=lambda: calc.numb_butt(6))
-b7 = Button(master, text='7', command=lambda: calc.numb_butt(7))
-b8 = Button(master, text='8', command=lambda: calc.numb_butt(8))
-b9 = Button(master, text='9', command=lambda: calc.numb_butt(9))
-b_dot = Button(master, text='', command=lambda: calc.numb_butt("."))
-
-plus = Button(master,text="+", command=lambda: calc.set_op(0))
-minus = Button(master,text="-", command=lambda: calc.set_op(1))
-times = Button(master,text="*", command=lambda: calc.set_op(2))
-Dives = Button(master,text="/", command=lambda: calc.set_op(3))
-Squared = Button(master,text="^2", command=lambda: calc.set_op(4))
-
-equals = Button(master,text="=", command=calc.equate)
-clear = Button(master,text="c", command=calc.clear)
-
-display.place(x=0, y=2)
-b7.grid(row=1, column=0)
-b8.grid(row=1, column=1)
-b9.grid(row=1,column=2)
-b4.grid(row=2,column=0)
-b5.grid(row=2,column=1)
-b6.grid(row=2,column=2)
-b1.grid(row=3,column=0)
-b2.grid(row=3,column=1)
-b3.grid(row=3,column=2)
-b0.grid(row=4,column=0)
-
-b_dot.grid(row=4,column=1)
-clear.grid(row=4,column=2)
-
-plus.grid(row=0,column=3)
-minus.grid(row=1,column=3)
-times.grid(row=2,column=3)
-Dives.grid(row=3,column=3)
-equals.grid(row=4,column=3)
-Squared.grid(row=0, column=4)
-
-master.mainloop()
-
+root = tk.Tk()
+root.title("Graphing Calculator")
+btn_list = [
+    '7', '8', '9', '*', 'C',
+    '4', '5', '6', '/', 'M->',
+    '1', '2', '3', '-', '->M',
+    '0', '.', '=', '+', 'neg']
+# create all buttons with a loop
+r = 1
+c = 0
+for b in btn_list:
+    rel = 'ridge'
+    cmd = lambda x=b: click(x)
+    tk.Button(root, text=b, width=5, relief=rel, command=cmd).grid(row=r, column=c)
+    c += 1
+    if c > 4:
+        c = 0
+        r += 1
+# use Entry widget for an editable display
+entry = tk.Entry(root, width=33, bg='dark green')
+entry.grid(row=0, column=0, columnspan=5)
+root.mainloop()
